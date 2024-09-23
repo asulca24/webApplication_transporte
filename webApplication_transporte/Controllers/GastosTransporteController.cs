e695bf2b63296113a79131f4e6d47b6e42b94461
lmase <asulca24@gmail.com>
2024-09-23 16:25:59 -0500

    Add project files.

---
+++ webApplication_transporte/Controllers/GastosTransporteController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webApplication_transporte.Models;
+
+
+namespace webApplication_transporte.Controllers
+{
+    public class GastosTransporteController : Controller
+    {
+        // GET: GastosTransporte
+        public ActionResult Costos()
+        {
+            return View(new Transporte());
+        }
+        [HttpPost]public ActionResult Costos(Transporte reg)
+        {
+            ViewBag.costo = reg.Costo().ToString("######.#0");
+            ViewBag.seguro = reg.Seguro().ToString("######.#0");
+            ViewBag.monto = reg.Monto().ToString("######.#0");
+            ViewBag.igv = reg.IGV().ToString("######.#0");
+            ViewBag.total = reg.MontoTotal().ToString("######.#0");
+            return View(reg);
+        }
+        
+    }
+}
---
+++ webApplication_transporte/Models/Transporte.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webApplication_transporte.Models
+{
+    public class Transporte
+    {
+        public string num_guia { get; set; }
+        public DateTime fecha_guia { get; set; }
+        public string cliente {  get; set; }
+        public string carga {  get; set; }
+        public double TM { get; set; }
+        public double tarifa { get; set; }
+
+        public Transporte () {
+            num_guia = "";
+            fecha_guia= DateTime.Today;
+            cliente = "";
+            carga = "";
+            TM = 0;
+            tarifa = 150;
+        
+        }
+        public double Costo()
+        {
+            return tarifa * TM;
+        }
+        public double Seguro()
+        {
+            return 0.02 * Costo();
+        }
+        public double Monto()
+        {
+            return Costo() + Seguro();
+        }
+        public double IGV()
+        {
+            return 0.18 * Monto();
+        
+        }
+        public double MontoTotal()
+        {
+            return Monto() + IGV();        
+        }
+
+    }
+}
